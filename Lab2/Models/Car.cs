9f21558255001145e7addaae19ebab32d3dfcc01
ThreeAi <alexandrovaa2003@gmail.com>
2024-03-06 02:22:09 +0300

    init commit

---
+++ Lab2/Models/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    internal abstract class Car
+    {
+        protected double weight { init; get; }
+        protected double enginePower { init; get; }
+        protected string name { init; get; }
+
+        public static int numberOfCars;
+
+        public Car(string name, double weight, double enginePower)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.enginePower = enginePower;
+            numberOfCars++;
+        }
+
+        public abstract double mileage();
+        public virtual double racing() { return enginePower/weight; }
+    }
+}
---
+++ Lab2/Models/ElecticCar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    internal class ElecticCar : Car
+    {
+        private double batteryCapacity { init; get; }
+
+        public ElecticCar(string name, double weight, double enginePower, double batteryCapacity) 
+            : base(name, weight, enginePower)
+        {
+            this.batteryCapacity = batteryCapacity;
+        }
+
+        public override sealed double mileage()
+        {
+            return this.batteryCapacity * this.enginePower / this.weight;
+        }
+
+        public override double racing()
+        {
+            return this.batteryCapacity * 0.3 * this.enginePower / this.weight;
+        }
+    }
+}
